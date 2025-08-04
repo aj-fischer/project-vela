544b8e18f5f7d135de192d5b197ef083a13fa5e0
AJ Fischer <agejfischer@gmail.com>
2025-08-04 19:36:02 -0400

    Record environmental sensor data from BME680 sensor

--- src/main.py
+++ src/main.py
@@ -6,27 +6,49 @@ from datetime import datetime
 
 # External modules
 import board
-import adafruit_bh1750
+import busio
+import adafruit_bme680  # Environmental sensor.
+import adafruit_bh1750  # Light (lux) sensor.
 
 
 def main():
     # Set pi board to use I2C communication.
     i2c = board.I2C()
 
-    # BH1750 is the light sensor.
-    sensor = adafruit_bh1750.BH1750(i2c)
+    # Create light sensor object.
+    #bh1750 = adafruit_bh1750.BH1750(i2c)
+
+    # Create environmental sensor object.
+    bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+    # Set sea level pressure (in hPa) for altitude reading.
+    bme680.sea_level_pressure = 1013.25
     
     # Get current local date and time.
     current_datetime = datetime.now()
 
-    # Round lux value to two decimals for readability.
-    rounded_lux = round(sensor.lux, 2)
+    # Round light sensor values (lux) for readability.
+    #rounded_lux = round(bh1750.lux, 2)
+
+    # Round environmental sensor values for readability.
+    rounded_temp = round(bme680.temperature, 1)
+    rounded_gas = round(bme680.gas, 1)
+    rounded_humidity = round(bme680.humidity, 1)
+    rounded_pressure = round(bme680.pressure, 1)
+    rounded_altitude = round(bme680.altitude, 2)
     
-    # Save rounded lux reading (light intensity) as well as timestamp.
-    data_row = [current_datetime, rounded_lux]
+    # Save rounded environmental sensor values as well as timestamp.
+    data_row = [
+            current_datetime,
+            rounded_temp,
+            rounded_gas,
+            rounded_humidity,
+            rounded_pressure,
+            rounded_altitude
+    ]
     
     # File name for data file (csv).
-    csv_file = "/home/aj/Projects/project-vela/sensor_data.csv"
+    csv_file = "/home/aj/Projects/project-vela/bme680_data.csv"
 
     # Check if file already exists.
     does_file_exist = os.path.isfile(csv_file)
@@ -37,7 +59,14 @@ def main():
 
         # If the file is new, write the header.
         if not does_file_exist:
-            writer.writerow(['Timestamp', 'LUX'])
+            writer.writerow([
+                'Timestamp',
+                'Temperature (C)',
+                'Gas (Ohms)',
+                'Humidity (%)',
+                'Pressure (hPa)',
+                'Altitude (m)'
+            ])
 
         # Write the data row.
         writer.writerow(data_row)
